89808ddfb947776af67e4b1fd0e3e49682ded287
rongxin.l <rongixn.l@mihoyo.com>
2022-12-08 10:37:06 +0800

    add

--- hook_test.go
+++ hook_test.go
@@ -10,9 +10,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	. "github.com/sirupsen/logrus"
-	"github.com/sirupsen/logrus/hooks/test"
-	. "github.com/sirupsen/logrus/internal/testutils"
+	. "github.com/sftfjugg/logrus"
+	"github.com/sftfjugg/logrus/hooks/test"
+	. "github.com/sftfjugg/logrus/internal/testutils"
 )
 
 type TestHook struct {
